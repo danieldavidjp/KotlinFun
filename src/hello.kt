c9395ecdb8e224a541eb9d6758399e94269432b8
Daniel Jimenez <danieldavidjp@gmail.com>
2021-07-22 18:26:04 -0500

    Maps

--- src/hello.kt
+++ src/hello.kt
@@ -1,25 +1,15 @@
 fun main() {
-    // For loops
+    // Maps
     /*
-    for(x in 1..10){
-        println(x)
-    }
+    var dogs = mutableMapOf("Nina" to 14, "Cirilo" to 7, "Lulo" to 2)
 
+    println(dogs["Nina"])
+
+    dogs["Balto"] = 52
     */
-    /*var favCandies = listOf("KitKat","Jet","Skittles")
 
-    for(y in favCandies){
+    var slangs = mapOf("vecino" to "Person that you dont know", "Achichay" to "Is Really Cold")
 
-        println(y)
-    }
+    println(slangs["vecino"])
 
-    */
-    //Loop from 1 to 200 and print out all odd numbers
-    for(x in 1..200){
-
-        if(x%2 != 0){
-            println(x)
-        }
-
-    }
 }
